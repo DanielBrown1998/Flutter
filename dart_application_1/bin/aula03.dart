fccfc7d81fa08b32c847bc5ee67dc4455d39ce41
Daniel <daniel_academico1998@hotmail.com>
2024-08-09 09:28:17 -0300

    estudando dart

---
+++ dart_application_1/bin/aula03.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:dart_application_1/aula_03.dart';
+
+void main(List<String> arguments) {
+  print('Main da Aula 03');
+  fun_03();
+  print(multi(2, 3));
+  print(div(2, 3));
+  print(sub(2, 3));
+  print(sum(2, 3));
+  print(() {
+    return 'Função anônima';
+  });
+  seusDados('Fulano', 30, celular: '99-99999-9999');
+}
