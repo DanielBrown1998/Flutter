34cab466dc200dc14b4d11e339fb167f63f04b72
Daniel <daniel_academico1998@hotmail.com>
2024-08-12 18:06:08 -0300

    refactor

--- 0.0.1/flutter_application_1/android/app/src/main/kotlin/br/com/danielapps/flutter_application_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package br.com.danielapps.flutter_application_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 0.0.1/flutter_application_1/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app/my_app.dart';
-
-//aprendendo flutter e dart - 2024
-void main() {
-  runApp(const MyApp());
-}
-
