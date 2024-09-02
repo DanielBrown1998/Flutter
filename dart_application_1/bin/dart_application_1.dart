b76913db0acd35e8f8c5ff37ab8ea4383d09cd5e
Daniel <daniel_academico1998@hotmail.com>
2024-09-02 15:08:54 -0300

    estudando dart

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -3,4 +3,19 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+
+  var pessoa = dart_application_1.Pessoa(
+    nome: 'João Paulo',
+    idade: 30,
+    altura: 1.80,
+    peso: 80.0,
+  );
+  List<int> results = [1, 2, 3, 4, 5];
+  int max = 0;
+  results.forEach((int element) {
+    if (element > max) {
+      max = element;
+    }
+  });
+  print(max);
 }
