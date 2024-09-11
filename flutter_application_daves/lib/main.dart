f4ab45a1cb367e8cd23d6a7c9e3308c16159ac8c
Daniel <daniel_academico1998@hotmail.com>
2024-09-11 09:14:15 -0300

    estudando flutter

--- flutter_application_daves/lib/main.dart
+++ flutter_application_daves/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Hello World'),
@@ -31,6 +39,19 @@ void main() {
           child: Text('Hello World'),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home();
+  }
+}
+
+void main() {
+  runApp(const MyApp());
 }
