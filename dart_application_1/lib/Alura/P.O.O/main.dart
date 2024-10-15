5166aa4de98c208ccdb9e07fe93e4375e84147f5
Daniel <daniel_academico1998@hotmail.com>
2024-10-15 20:56:10 -0300

    estudando collections

--- dart_application_1/lib/Alura/P.O.O/main.dart
+++ dart_application_1/lib/Alura/P.O.O/main.dart
@@ -1,17 +1,19 @@
 import 'classes.dart' as classes;
 
+enum Transporte { carro, aviao, barco, bicicleta }
 
-enum Transporte{
-  carro,
-  aviao,
-  barco,
-  bicicleta
+Set<dynamic> gerandoSets(List<dynamic> lista) {
+  Set<dynamic> meuSet = {};
+  for (dynamic item in lista) {
+    if (!meuSet.contains(item)) {
+      meuSet.add(item);
+    }
+  }
+  return meuSet;
 }
 
-
-void escolherMeioTransporte(Transporte meioTransporte){
-
-  switch (meioTransporte){
+void escolherMeioTransporte(Transporte meioTransporte) {
+  switch (meioTransporte) {
     case Transporte.carro:
       print("Carro");
       break;
@@ -27,9 +29,9 @@ void escolherMeioTransporte(Transporte meioTransporte){
   }
 }
 
-
 void main() {
-  escolherMeioTransporte(Transporte.carro)
-  
+  escolherMeioTransporte(Transporte.carro);
+  List<dynamic> dados = ['Daniel', 26, '26510700', 2000.00, true, classes.BoloChocolate(nome: 'Casadinho', peso: 1, isPrecisaCozimento: false, isVegano: false, sabor: 'doce'), "Daniel", "Brown", "Brown"];
+  Set<dynamic> meuSet = gerandoSets(dados);
+  print(meuSet);
 }
-
