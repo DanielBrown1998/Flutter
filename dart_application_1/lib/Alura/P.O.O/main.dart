f0d0c40b1cdb7dec6b797e1d32ba67ab366cadfc
Daniel <daniel_academico1998@hotmail.com>
2024-10-15 19:38:34 -0300

    estudando enums e switchs

--- dart_application_1/lib/Alura/P.O.O/main.dart
+++ dart_application_1/lib/Alura/P.O.O/main.dart
@@ -1,9 +1,35 @@
 import 'classes.dart' as classes;
 
+
+enum Transporte{
+  carro,
+  aviao,
+  barco,
+  bicicleta
+}
+
+
+void escolherMeioTransporte(Transporte meioTransporte){
+
+  switch (meioTransporte){
+    case Transporte.carro:
+      print("Carro");
+      break;
+    case Transporte.aviao:
+      print("Avião");
+      break;
+    case Transporte.barco:
+      print("Barco");
+      break;
+    case Transporte.bicicleta:
+      print("Bicicleta");
+      break;
+  }
+}
+
+
 void main() {
-  const String nome = 'Daniel';
-  late String sobreNome;
-  sobreNome = 'Morais';
-  print('Nome: $nome $sobreNome');
+  escolherMeioTransporte(Transporte.carro)
+  
 }
 
