17ec5d9bb16950ae10853991803921556b6fb95c
Daniel <daniel_academico1998@hotmail.com>
2024-10-15 19:10:13 -0300

    "Removed BoloChocolate instance creation and method calls, replaced with string variable declarations and print statement."

--- dart_application_1/lib/Alura/P.O.O/main.dart
+++ dart_application_1/lib/Alura/P.O.O/main.dart
@@ -1,15 +1,9 @@
 import 'classes.dart' as classes;
 
 void main() {
-  var bolo = classes.BoloChocolate(
-    nome: 'Bolo de chocolate',
-    peso: 1.5,
-    sabor: 'doce',
-    isVegano: false,
-  );
-
-  bolo.propriedades();
-  bolo.cozinhar();
-  bolo.assar();
+  const String nome = 'Daniel';
+  late String sobreNome;
+  sobreNome = 'Morais';
+  print('Nome: $nome $sobreNome');
 }
 
