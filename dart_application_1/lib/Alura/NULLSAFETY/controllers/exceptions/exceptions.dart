06a2ea3be97dc2fa9544173ffe887f1ac3d87769
Daniel <daniel_academico1998@hotmail.com>
2024-10-28 19:15:04 -0300

    Adiciona tratamento de exceções e mensagens de erro

--- dart_application_1/lib/Alura/NULLSAFETY/controllers/exceptions/exceptions.dart
+++ dart_application_1/lib/Alura/NULLSAFETY/controllers/exceptions/exceptions.dart
@@ -14,3 +14,4 @@ class InsufficientBalanceError implements Exception{
   }
 }
 
+
--- dart_application_1/lib/Alura/NULLSAFETY/main.dart
+++ dart_application_1/lib/Alura/NULLSAFETY/main.dart
@@ -1,6 +1,6 @@
-
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
+import 'controllers/exceptions/exceptions.dart' as exceptions;
 
 void main() {
 //Criando o banco
@@ -8,21 +8,29 @@ void main() {
 
 // Adicionando contas
   bankController.addAccount(
-    id: "Ricarth",
-    account:
-      Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+      id: "Ricarth",
+      account:
+          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
 
   bankController.addAccount(
-    id: "Kako",
-    account:
-      Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+      id: "Kako",
+      account:
+          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
 
 // Fazendo transferência
-  bool result = bankController.makeTransfer(
-    idSender: "Kako", idReceiver: "Ricarth", amount: 600);
-
+  try {
+    bool result = bankController.makeTransfer(
+        idSender: "Kako", idReceiver: "Ricarth1", amount: 600);
 // Observando resultado
-  print(result);
-}
+    print('Transferência finalizada');
+    print(result);
 
- 
+  } on exceptions.IdError catch (e) {
+    print(e.message);
+  } on exceptions.InsufficientBalanceError catch (e) {
+    print(e.message);
+  } catch (e) {
+    print('houve um erro inesperado!');
+    print('tente novamente mais tarde...');
+  }
+}
